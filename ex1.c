e4af66dceb68836159945f7d9d6d855dc69e130d
Landon Davidson <accounts@landond.tech>
2025-01-09 19:08:15 -0800

    Add: InsertSort and print statements

--- ex1.c
+++ ex1.c
@@ -1,10 +1,21 @@
 // Landon Davidson
 #include <inttypes.h>
+#include <stdio.h>
+
+void InsertSort(int64_t arr[], int size) {
+    for (int i = size - 1; i > 0; i--) {
+        if (arr[i] < arr[i - 1]) {
+            int64_t tmp = arr[i - 1];
+            arr[i - 1] = arr[i];
+            arr[i] = tmp;
+        }
+    }
+}
 
 void CopyAndSort(int64_t src[], int64_t dst[], int size) {
     for (int i = 0; i < size; i++) {
         dst[i] = src[i];
-        //TODO InsertSort function
+        InsertSort(dst, i + 1);
     }
 }
 
@@ -13,4 +24,10 @@ int main() {
     int64_t src[] = {3, 2, -5, 7, 17, 42, 6, 333, 7, 8, -8, 6};
     int64_t dst[ARR_SIZE];
     CopyAndSort(src, dst, ARR_SIZE);
+
+    // Print out sorted array
+    printf("The sorted array is: ");
+    for (int i = 0; i < ARR_SIZE; i ++) {
+        printf("%" PRId64 ", ", dst[i]);
+    }
 }
