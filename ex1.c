08bacaf34a923c9373519aa9853bb9b71649756a
Landon Davidson <accounts@landond.tech>
2025-01-09 19:13:11 -0800

    Refactor: add comments

--- ex1.c
+++ ex1.c
@@ -2,17 +2,23 @@
 #include <inttypes.h>
 #include <stdio.h>
 
+// Takes an array and array's size as input and performs one pass of an insertion sort
 void InsertSort(int64_t arr[], int size) {
+    // Start at back of array and repeatedly swap if the preceding element is smaller,
+    // otherwise return since the new element is fully sorted
     for (int i = size - 1; i > 0; i--) {
         if (arr[i] < arr[i - 1]) {
             int64_t tmp = arr[i - 1];
             arr[i - 1] = arr[i];
             arr[i] = tmp;
-        }
+        // If the preceding element is smaller than the new element is at the correct position and sorting is done
+        } else return;
     }
 }
 
-void CopyAndSort(int64_t src[], int64_t dst[], int size) {
+// Takes a source and destination array and their size as input then copies element and sorts each element one at a time
+void CopyAndSort(int64_t src[], int64_t dst[], int size) { // NOLINT(*-non-const-parameter)
+    // For each element in src, copy to dst and call InsertSort which does one pass of insertion sort
     for (int i = 0; i < size; i++) {
         dst[i] = src[i];
         InsertSort(dst, i + 1);
@@ -21,6 +27,7 @@ void CopyAndSort(int64_t src[], int64_t dst[], int size) {
 
 #define ARR_SIZE 12
 int main() {
+    // Initialize the src and dst arrays and call CopyAndSort
     int64_t src[] = {3, 2, -5, 7, 17, 42, 6, 333, 7, 8, -8, 6};
     int64_t dst[ARR_SIZE];
     CopyAndSort(src, dst, ARR_SIZE);
